67275e3061c3694befa8402492e6eca159940078
Angel Velasquez <angelvelasquez@Angels-MacBook-Pro-171.local>
2023-04-10 20:51:22 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import PrimeVue from "primevue/config";
+
+
+// App Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+// Add PrimeFlex
+import 'primeflex/primeflex.css';
+
+// Add Components
+import Card from "primevue/card";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Toolbar from "primevue/toolbar";
+
+
+createApp(App)
+    .use(PrimeVue, { ripple: true })
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar)
+    .mount('#app')
