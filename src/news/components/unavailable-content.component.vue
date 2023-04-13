213ee6103d591f38cc8aec2fca500ad836f05b6f
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-12 20:08:00 -0500

    feat: implemented unavailable content view

---
+++ src/news/components/unavailable-content.component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div><h4>News Service is unavailable now</h4></div>
+      <div v-for="error in errors"><h6>{{ error }}</h6></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "unavailable-content",
+    props: {
+        errors: null,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
