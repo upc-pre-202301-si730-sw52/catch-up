eb5b36f28ec647551f6086344ebcb40d42282345
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-12 20:11:55 -0500

    feat: implemented footer content section

---
+++ src/news/components/footer-content.component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <div>
+          <p>Copyright &copy; 2023. ACME Studios</p>
+      </div>
+      <div>
+          <p>Made with love by ACME Studios using Vue</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "footer-content"
+}
+</script>
+
+<style scoped>
+
+</style>
